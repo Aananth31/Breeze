f3f44bd01d7af983bbeff671f539d180d841101d
Aananth V <aananth2001@gmail.com>
2014-03-27 16:44:12 +0530

    Update commands.js

--- commands.js
+++ commands.js
@@ -73,6 +73,25 @@ var commands = exports.commands = {
 		}
 	},
 
+	restart: function(target, room, user) {
+		if (!this.can('lockdown')) return false;
+		try {
+			var forever = require('forever'); 
+		} catch(e) {
+			return this.sendReply('/restart requires the "forever" module.');
+		}
+
+		if (!Rooms.global.lockdown) {
+			return this.sendReply('For safety reasons, /restart can only be used during lockdown.');
+		}
+
+		if (CommandParser.updateServerLock) {
+			return this.sendReply('Wait for /updateserver to finish before using /restart.');
+		}
+		this.logModCommand(user.name + ' used /restart');
+		Rooms.global.send('|refresh|');
+		forever.restart('app.js');
+	},
 	
 	eating: 'away',
 	gaming: 'away',
