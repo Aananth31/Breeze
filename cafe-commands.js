7d134cfc1ca81b922653fce02b34c5afe6cea92e
Chaarizard <aananth2001@gmail.com>
2014-03-29 19:34:07 +0530

    Update cafe-commands.js

--- cafe-commands.js
+++ cafe-commands.js
@@ -1,4 +1,4 @@
-exports.cafecommands = {
+exports.cafecommands = function () {
   writeMoney: function(uid, amount) {
 			var data = fs.readFileSync('config/money.csv','utf8')
 			var match = false;
