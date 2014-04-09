be2976658ae15e11a2a4ff5984713d5bbf97c0a3
小太 <nospam@xiaotai.org>
2014-04-09 19:06:49 +1000

    Add /pickrandom

--- config/commands.js
+++ config/commands.js
@@ -1062,6 +1062,16 @@ var commands = exports.commands = {
 	},
 
 	/*********************************************************
+	 * Custom commands
+	 *********************************************************/
+
+	pr: 'pickrandom',
+	pickrandom: function(target, room, user) {
+		if (!this.canBroadcast()) return false;
+		return this.sendReply(target.split(',').map(function (s) { return s.trim(); }).randomize()[0]);
+	},
+
+	/*********************************************************
 	 * Help commands
 	 *********************************************************/
 
