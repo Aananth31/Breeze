f5160135146ef200450f8bc0654dd262654999f6
PokerKid <kkaito1412.kk@gmail.com>
2014-04-19 01:41:39 +0800

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -1584,6 +1584,10 @@ var commands = exports.commands = {
 	 empo: function(target, room, user) {
                 if (!this.canBroadcast()) return;
                 this.sendReplyBox('<center><img src="http://i.imgur.com/IHd5yRT.gif"><br><img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/piplup.gif"><img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/prinplup.gif"><img src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/empoleon.gif"><br><font color=blue>The control on yourself is raised when you evolve your way to think.<br>Ace: Empoleon');
-        }
+        },
+        jolteon64: function(target,room,user) {
+	 	if (!this.canBroadcast()) return;
+	 	this.sendReplyBox('Fuck me')
+	 }
 
 };
