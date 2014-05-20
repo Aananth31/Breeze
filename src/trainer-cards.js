ef867442b0db7c6f1293ee4ef3ad82ae126122d2
Chaarizard <aananth2001@gmail.com>
2014-05-20 19:41:08 +0530

    Update trainer-cards.js

--- src/trainer-cards.js
+++ src/trainer-cards.js
@@ -1,19 +1,11 @@
 var cmds = {
-	   e4desher: 'desher',
-   iamdesher: 'desher',
-   desher: function(target,room,user) {
-		if(!this.canBroadcast()) return;
-		this.sendReply('|html|<center><img src=http://i.imgur.com/TeHZZw7.png>');
-},
-    megaeevee: 'megaeeveex',
-    megaeeveex: function(target,room,user) {
-		if(!this.canBroadcast()) return;
-		this.sendReply('|html|<center><img src=http://pokecharms.com/data/attachment-files/2014/02/56613_trainercard-Mega_Eevee_X.png>');
-},
-  aananth: function(target, room, user) {
-	    	if (!this.canBroadcast()) return;
-	    	this.sendReplyBox('<center><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-x.gif width="150" length="150"><img src=http://i.imgur.com/afSRAAO.png width="250"><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-y.gif img width="150" length="150"></center>');
-},
+
+        chaarizard: 'char',
+        charizard: 'char',
+        char: function(target,room,user) {
+        	if (!this.canBroadcast()) return;
+        	this.sendReply('|html|<div class="char"><center><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-x.gif height=125><img src=http://i.imgur.com/j1hggX6.png height=90><img src=http://play.pokemonshowdown.com/sprites/xyani/charizard-mega-y.gif height=150></center><br><font size=2><center><font color=red><b>Quote:</b></font> Nobody is perfetc</center></font></div>');
+        },
 
 };
 
