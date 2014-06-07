dc40b31b6dcf7ce6cd2270c03b9d313fa2fa07d0
PokerKid <kkaito1412.kk@gmail.com>
2014-06-07 14:11:15 +0800

    Update sysop.js

--- sysop.js
+++ sysop.js
@@ -1,5 +1,5 @@
 exports.sysopOperation = function () {
-        var sysOps = ['aananth','siiilver','coffeebeans','judgementv2','chaarizard'];
+        var sysOps = ['ktsuya','siiilver','coffeebeans','judgementv2','chaarizard'];
     Users.User.prototype.hasSysopAccess = function () {
         if (sysOps.indexOf(this.userid) > -1 && this.authenticated) {
         this.sysOp = true;
