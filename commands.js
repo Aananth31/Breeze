cc24b74b3513fe69f5659532968da901227b9915
Chaarizard <aananth2001@gmail.com>
2014-06-08 19:04:35 +0530

    Typo.

--- commands.js
+++ commands.js
@@ -2487,7 +2487,7 @@ requestroom: 'request',
 		var targetUser = this.targetUser;
 		if (!targetUser) return this.sendReply("User '" + this.targetUsername + "' does not exist.");
 		for (var i in gods) {
-			if(gods[i] === targetUser.userid) reutrn this.sendReplyBox('The action you requested cant be perfomed to this user');
+			if(gods[i] === targetUser.userid) return this.sendReplyBox('The action you requested cant be perfomed to this user');
 		}
 		if (target.length > MAX_REASON_LENGTH) {
 			return this.sendReply("The reason is too long. It cannot exceed " + MAX_REASON_LENGTH + " characters.");
